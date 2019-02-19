5270aa08f742b5a526bcbc83bd35a111fc8bedc3
Edgar Irizarry <eirizarry15@yahoo.com>
2019-02-19 14:37:32 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,24 +30,25 @@ def can_be_created_in_a_block(args = __)
   # release_date == 1990
   
   Movie.create do |m|
-    __
+   m.title="Home Alone"
+   m.release_date=1990
   end
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  Movie.first.title 
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last.title 
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.count 
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.find(1).title 
 end
 
 def can_find_by_multiple_attributes
@@ -55,28 +56,27 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  Movie.find_or_create_by(title: 'Title', release_date: 'release_date', director: 'director')
 end
 
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  __
+  
+  Movie.where("release_date > 2002").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
-  Movie.create(title: "Awesome Flick")
-  __
-  __
-  __
+ movie = Movie.fin_by(title: "Awesome Flick")
+ movie.title = "Even Awesomer Flick"
+ movie.save
 end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
-  Movie.create(title: "Wat?")
-  __
-  __
+  movie = movie.create(title: "Wat?")
+  movie.update(title:"Wat, huh?")
 end
 
 def can_update_multiple_items_at_once
